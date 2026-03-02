ec6d22331e77e9294a93cdd76ae0353e8821b77d
sreenithi07 <sreenithi.k07@gmail.com>
2026-03-02 10:09:51 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,32 @@
 package com.example;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void testGradeA() {
+        assertEquals("A", App.calculateGrade(95));
+    }
+
+    @Test
+    public void testGradeB() {
+        assertEquals("B", App.calculateGrade(80));
+    }
+
+    @Test
+    public void testGradeC() {
+        assertEquals("C", App.calculateGrade(65));
+    }
+
+    @Test
+    public void testGradeD() {
+        assertEquals("D", App.calculateGrade(55));
+    }
+
+    @Test
+    public void testGradeF() {
+        assertEquals("F", App.calculateGrade(40));
     }
 }
